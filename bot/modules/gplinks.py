3e95c4429ecbdc4998173c47e884ae10418dcdef
kanger-007 <67798221+kanger-007@users.noreply.github.com>
2022-03-11 12:16:07 +0530

    Update gplinks.py

--- bot/modules/gplinks.py
+++ bot/modules/gplinks.py
@@ -16,36 +16,58 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-def gplinks(update ,context):
-    args = update.message.text.split(" ", maxsplit=1)
-    reply_to = update.message.reply_to_message
-    if len(args) > 1:
-         link = args[1]
-    elif reply_to is not None:
-         link = reply_to.text
-    else:
-        link = ''
-    gp_link = is_gp_link(link)
-    if gp_link:
-       client = requests.Session()
-       res = client.get(link)
-       
-       bs4 = BeautifulSoup(res.content, 'lxml')
-       inputs = bs4.find_all('input')
-       data = { input.get('name'): input.get('value') for input in inputs }
+import time
+import requests
+import cloudscraper
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import argparse
 
-       h = {
+arguments = argparse.ArgumentParser()
+arguments.add_argument("-url", "--url", dest="url", help="urllink")
+args = arguments.parse_args()
+url = args.url # eg: https://gplinks.co/XXXX
+
+# ==============================================
+
+def gplinks_bypass(url):
+    scraper = cloudscraper.create_scraper(allow_brotli=False)
+    res = scraper.get(url)
+    
+    h = { "referer": res.url }
+    res = scraper.get(url, headers=h)
+    
+    bs4 = BeautifulSoup(res.content, 'lxml')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
         'content-type': 'application/x-www-form-urlencoded',
         'x-requested-with': 'XMLHttpRequest'
     }
-       time.sleep(10)
-       p = urlparse(link)
-       final_url = f'{p.scheme}://{p.netloc}/links/go'
-       res = client.post(final_url, data=data, headers=h).json()
+    
+    time.sleep(10) # !important
+    
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+    res = scraper.post(final_url, data=data, headers=h).json()
 
-       return res
-      
+    return res
 
+# ==============================================
+
+print(gplinks_bypass(url))
+
+# ==============================================
+
+'''
+SAMPLE OUTPUT:
+{
+    'status': 'success', 
+    'message': 'XXXXXX', 
+    'url': 'XXXX' (Bypassed URL)
+}
+'''
     
 gplink_handler = CommandHandler(BotCommands.GplinkCommand, gplinks, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(gplink_handler)
